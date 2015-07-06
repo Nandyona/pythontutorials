7d711852b1527a793812699ff6aea958d26cf99d
Nandyona <Gertrude.Nandyona@devinit.org>
2015-07-06 18:53:51 +0300

    Python Tutorials
    Python lessons by Allan Lukwago

---
+++ flow.py
@@ -0,0 +1,13 @@
+'''program flow
+'''
+age=input("my age:")
+name="Gertrude:"
+if age == 24:	
+	print("age is 24")
+	b=age+7
+	print(b)
+if age <24:
+	print("you rae allowed in here")
+else:
+	print("please get out")
+	print("name is {0} and {1}".format(name,age))
---
+++ hello.py
@@ -0,0 +1,3 @@
+print("my name is Gertrude Mariah Nandyona")
+age=input()
+print(age*2)

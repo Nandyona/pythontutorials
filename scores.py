b010b78b7944863dd048c344edb88499ca1f9a6d
Nandyona <Gertrude.Nandyona@devinit.org>
2015-07-13 13:32:28 +0300

    Exam scores and Grades
    Grading Students grades

---
+++ scores.py
@@ -0,0 +1,37 @@
+def exam_scores():
+
+    # Exam score and grades
+
+    print("Welcome to the exam score  for your Maths,Science and English subjects ")
+
+    print("enter your marks and view your scores")
+
+    name=raw_input ('What is you name: ')
+
+    maths_score=input (" Enter your Maths Marks" )
+    Science_score=input ("Enter your Science Marks")
+    english_score= input (" Enter your English marks:")
+   
+score=("english marks, maths marks, Science marks":)
+  
+    for x in score:
+        if  x>= 95:
+            grade="A"
+        elif  x >= 75 and x < 95:
+            grade="B"
+
+            x >60 and x <75  
+            grade="C"
+
+            x>45 and x<60
+            grade="D"
+
+          else: 
+            grade "F"  
+       
+        print("you got grade {0} for score {1}".format(grade,x))
+
+        Average=
+
+        Total=
+        
